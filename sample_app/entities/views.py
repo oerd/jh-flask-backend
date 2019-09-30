8956731b59b1b5024ec3d6a0abb350545ab1592e
Oerd Cukalla <rosaccu@gmail.com>
2019-09-30 18:18:21 +0200

    Refactor modelview API to utility modelview

--- sample_app/entities/views.py
+++ sample_app/entities/views.py
@@ -1,107 +1,16 @@
-from flask import request
-from flask.views import MethodView
-from werkzeug.exceptions import InternalServerError
-from marshmallow import ValidationError
-from pprint import pprint
-
+from utils.web import BaseApi, BaseItemsApi
 from .models import BankAccount, Operation, Label
 from .schema import BankAccountSchema, OperationSchema, LabelSchema
 
 
-# FIXME: Refactor this away!
-class BankAccountsApi(MethodView):
-    """ Example of a class inheriting from flask.views.MethodView
-
-    All 5 request methods are available at /api/example/<entity>
-    """
-    schema = BankAccountSchema(many=True)
-
-    def get(self):
-        """ Responds to GET requests """
-        result = BankAccount.query.all()
-        return self.schema.jsonify(result), 200
-
-    def post(self):
-        """ Responds to POST requests """
-        item = None
-        pprint(request.json)
-        try:
-            item = BankAccountSchema().load(request.json)
-        except ValidationError as err:
-            pprint(item)
-            pprint(err.messages)
-        else:
-            item.save()
-        return self.schema.jsonify(item), 200
-
-    def put(self):
-        """ Responds to PUT requests """
-        return "Responding to a PUT request"
+class BankAccountsApi(BaseApi):
+    Model = BankAccount
+    Schema = BankAccountSchema
 
 
-class BankAccountItemsApi(MethodView):
-    schema = BankAccountSchema()
-
-    def get(self, entity):
-        """ getBankAccount() """
-        account = BankAccount.query.get_or_404(entity)
-        return self.schema.jsonify(account), 200
-
-    def delete(self, entity):
-        """ deleteBankAccount """
-        result = BankAccount.query.get_or_404(entity)
-        if result.delete():
-            return "", 200
-        raise InternalServerError
-
-
-class BaseApi(MethodView):
-    """ Example of a class inheriting from flask.views.MethodView
-
-    All 3 request methods are available at /api/example
-    """
-    def __init__(self, **kwargs):
-        super(BaseApi, self).__init__(**kwargs)
-
-    def get(self):
-        """ getAllEntities() """
-        result = self.Model.query.all()
-        return self.Schema(many=True).jsonify(result), 200
-
-    def post(self):
-        """ createEntity() """
-        try:
-            item = self.Schema().load(request.json)
-        except ValidationError as err:
-            pprint(err.messages)
-            raise InternalServerError
-        else:
-            item.save()
-            return self.Schema().jsonify(item), 200
-
-    def put(self, entity):
-        """ updateEntity() """
-        return "Responding to a PUT request"
-
-
-class BaseItemsApi(MethodView):
-    """ Base class to handle item APIs
-
-     treat `/api/entity/<int:entity_id>`-style URLs """
-    def __init__(self, **kwargs):
-        super(BaseItemsApi, self).__init__(**kwargs)
-
-    def get(self, entity):
-        """ getEntity() """
-        result = self.Model.query.get_or_404(entity)
-        return self.schema.jsonify(result), 200
-
-    def delete(self, entity):
-        """ deleteEntity() """
-        result = self.Model.query.get_or_404(entity)
-        if result.delete():
-            return "", 200
-        raise InternalServerError
+class BankAccountItemsApi(BaseItemsApi):
+    Model = BankAccount
+    Schema = BankAccountSchema
 
 
 class LabelsApi(BaseApi):
--- utils/web.py
+++ utils/web.py
@@ -1,12 +1,68 @@
+from pprint import pprint
+
 from flask import request
 
 import logging
 
+from flask.views import MethodView
+from marshmallow import ValidationError
+from werkzeug.exceptions import InternalServerError
+
 logger = logging.getLogger('sample_app')
 
 
+# TODO: this might be superfluous, our API provides json, independently of HTTP request Content-Type
 def request_wants_json(ignore_rest=True):
     best = request.accept_mimetypes .best_match(['application/json', 'text/html'])
     if ignore_rest:
         return best == 'application/json'
     return best == 'application/json' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+
+class BaseApi(MethodView):
+    """ Example of a class inheriting from flask.views.MethodView
+
+    All 3 request methods are available at /api/example
+    """
+    def __init__(self, **kwargs):
+        super(BaseApi, self).__init__(**kwargs)
+
+    def get(self):
+        """ getAllEntities() """
+        result = self.Model.query.all()
+        return self.Schema(many=True).jsonify(result), 200
+
+    def post(self):
+        """ createEntity() """
+        try:
+            item = self.Schema().load(request.json)
+        except ValidationError as err:
+            pprint(err.messages)
+            raise InternalServerError
+        else:
+            item.save()
+            return self.Schema().jsonify(item), 200
+
+    def put(self, entity):
+        """ updateEntity() """
+        return "Responding to a PUT request"
+
+
+class BaseItemsApi(MethodView):
+    """ Base class to handle item APIs
+
+     treat `/api/entity/<int:entity_id>`-style URLs """
+    def __init__(self, **kwargs):
+        super(BaseItemsApi, self).__init__(**kwargs)
+
+    def get(self, entity):
+        """ getEntity() """
+        result = self.Model.query.get_or_404(entity)
+        return self.schema.jsonify(result), 200
+
+    def delete(self, entity):
+        """ deleteEntity() """
+        result = self.Model.query.get_or_404(entity)
+        if result.delete():
+            return "", 200
+        raise InternalServerError
